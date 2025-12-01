b585a6025715ea54a3471db25342c9a117ab8cc0
Dage10 <davidgelmacorral@gmail.com>
2025-12-01 17:38:08 +0100

    Exercici 1

---
+++ src/app/Components/correu/correu.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-correu',
+  imports: [],
+  templateUrl: './correu.html',
+  styleUrl: './correu.css',
+})
+export class Correu {
+  correu: any
+
+  constructor() {
+    this.correu = {
+      titol: "Pràctica Correu Angular8",
+      cos: "Hola aixo es un correu",
+      emisor: "david.gelma@institutvidreres.cat",
+      destinatari: "david.gelma@institutvidreres.cat"
+    }
+  }
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Correu} from './Components/correu/correu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Correu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
